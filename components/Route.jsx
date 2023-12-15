3d752f073bbc7809e158581cd3562954d0f594c8
sadik <sadikerdemir123@icloud.com>
2023-12-15 10:22:24 +0300

    routes eklendi

--- components/Route.jsx
+++ components/Route.jsx
@@ -1,83 +1,95 @@
-import React from 'react'
-import route from "../public/route.png"
-import Image from 'next/image'
+"use client"
+import React from "react";
+import route from "../public/route.png";
+import follow from "../public/follow1.png";
+import Image from "next/image";
+import { benefit, routes } from "@/utils/routeOptimization";
+import { motion } from "framer-motion";
 
 export default function Route() {
+    const fadeInAnimationVAriants = {
+        initial: {
+          opacity: 0,
+          y: 20,
+        },
+        animate: (index) => ({
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 0.04 * index,
+          },
+        }),
+      };
   return (
     <div className="bg-primary/30 xl:py-6 rounded-md mt-1 py-24 flex flex-col justify-center items-center gap-4 md:px-4 ">
-      <div className="flex flex-col md:flex-row justify-center items-center w-full gap-4 md:px-4 bg-red-100/20">
-        <div className='w-full'>
-            <h2>Rota Optimizasyonu</h2>
-        <Image src={route} width={250} height={250} alt="movitaqr" className="w-full" />
-        <p>
-        Mobil cihazlar ile saha personelinin işlerini ve araçların günlük rotalarını optimize ederek oluşturulan iş ve rota planlarını takip etmenizi sağlar.
-        <ol>
-            <li>
-            Mevcut araç kapasitelerini dikkate alarak gün içerisinde ürünlerin, kişilerin, kargoların, siparişlerin, sevkiyatların optimal rotasını planlamaktadır
-            </li>
-            <li>
-            Saha personelinin günlük rota planlarını oluşturur ve planlara uyulup uyulmadığını QR kod ve Personelin mobil uygulamalar üzerinden raporlaması ile takip etmenizi sağlamaktadır.
-            </li>
-            <li>
-            Gidilecek rotaları navigasyon hizmeti ile personelinize sunmaktadır.
-            </li>
-            <li>
-            Oluşturulan rota planları saha personel tarafından mobil uygulama üzerinden görüntülenebilmektedir.
-            </li>
-            <li>
-            Sisteme kaydedilen tüm rotalar seans ve araç bazlı olarak ayrı ayrı sunulmaktadır.
-            </li>
-            <li>
-            Sisteme kaydedilen tüm rotalar seans ve araç bazlı olarak ayrı ayrı sunulmaktadır.
-            </li>
-            <li>
-            Sisteme kaydedilen tüm rotalar seans ve araç bazlı olarak ayrı ayrı sunulmaktadır.
-            </li>
-            <li>
-            Oluşturulan güzergâhlara gerektiğinde yeni konumlar eklenerek güncel rota üzerinden planlama yapılabilmektedir.
-            </li>
-            <li>
-            Mobil uygulamadan belirlenen konuma ulaşım bilgisi uygulama üzerinden bildirilmektedir.
-            </li>
-        </ol>
-        </p>
+      <div className="flex flex-col justify-center items-center w-full gap-4 md:px-4 bg-red-100/20">
+        <div className="w-full mt-4">
+          <h2 className="my-2 text-center text-accent  font-bold text-lg">
+            Rota Optimizasyonu
+          </h2>
+          <div className="xl:flex-row flex-col flex gap-6 items-center justify-center text-md">
+            <Image
+              src={route}
+              width={350}
+              height={250}
+              alt="movitaqr"
+              className="w-full rounded-lg"
+            />
+            <div className="font-bold">
+              {routes.map((item, index) => (
+                <motion.div
+                  key={index}
+                  initial="initial"
+                  variants={fadeInAnimationVAriants}
+                  animate="animate"
+                  whileInView="animate"
+                  viewport={{
+                    once: true,
+                  }}
+                  custom={index}
+                  className="flex items-center gap-4"
+                >
+                  <div className="text-accent">{item.icon}</div>
+                  <div>{item.poKey}</div>
+                </motion.div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className='w-full flex flex-col'>
-            <h2>Sisteme Faydaları</h2>
-       
-        <ol>
-            <li>
-            Şirketin araç filosunun/saha personelinin en uygun şekilde yönetilmesini sağlar.
-            </li>
-            <li>
-            Şirket kaynaklarının verimli/etkin kullanılmasını sağlar.
-            </li>
-            <li>
-            Yakıttan tasarruf sağlar.
-            </li>
-            <li>
-            Hizmetlerin belirli bir kalitede ve standartta verilmesini sağlar.
-            </li>
-            <li>
-            Kullanım kolaylığı sayesinde kullanıcı dostudur.
-            </li>
-            <li>
-            Zamandan tasarruf sağlayarak müşterilerinize ve ofis işlerinize odaklanmanızı sağlar.
-            </li>
-            <li>
-            Çevreye zararlı gaz salınımını azaltır.
-            </li>
-            <li>
-            Müşteri memnuniyetini arttırır.
-
-            </li>
-            <li>
-            Süreç yönetimini kolaylaştırır.
-            </li>
-        </ol>
-
-        <Image src={route} width={250} height={250} alt="movitaqr" className="w-full flex-1" />
+        <div className="w-full mb-6">
+          <h2 className="my-8 text-center text-accent  font-bold text-lg ">
+            Sağladığı Yararlar
+          </h2>
+          <div className="xl:flex-row flex-col flex gap-6 items-center justify-center text-md">
+            <div className="font-bold flex flex-col  gap-2">
+              {benefit.map((item, index) => (
+                <motion.div
+                  key={index}
+                  initial="initial"
+                  variants={fadeInAnimationVAriants}
+                  animate="animate"
+                  whileInView="animate"
+                  viewport={{
+                    once: true,
+                  }}
+                  custom={index}
+                  className="flex items-center gap-4"
+                >
+                  <div className="text-accent">{item.icon}</div>
+                  <div>{item.poKey}</div>
+                </motion.div>
+              ))}
+            </div>
+            <Image
+              src={follow}
+              width={400}
+              height={250}
+              alt="movitaqr"
+              className=" w-full h-full md:w-[50%] rounded-lg"
+            />
+          </div>
         </div>
-        </div></div> 
-  )
+      </div>
+    </div>
+  );
 }
---
+++ utils/routeOptimization.js
@@ -0,0 +1,101 @@
+import { MdOutlineDoubleArrow } from "react-icons/md";
+
+export  const routes = [
+    
+    {
+        id: 0,
+        poKey: "Mobil cihazlar ile saha personelinin işlerini ve araçların günlük rotalarını optimize ederek oluşturulan iş ve rota planlarını takip etmenizi sağlar.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 1,
+        poKey: "Mevcut araç kapasitelerini dikkate alarak gün içerisinde ürünlerin, kişilerin, kargoların, siparişlerin, sevkiyatların optimal rotasını planlamaktadır",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 2,
+        poKey: "Saha personelinin günlük rota planlarını oluşturur ve planlara uyulup uyulmadığını QR kod ve Personelin mobil uygulamalar üzerinden raporlaması ile takip etmenizi sağlamaktadır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 3,
+        poKey: "Saha personelinin günlük rota planlarını oluşturur ve planlara uyulup uyulmadığını QR kod ve Personelin mobil uygulamalar üzerinden raporlaması ile takip etmenizi sağlamaktadır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 4,
+        poKey: "Gidilecek rotaları navigasyon hizmeti ile personelinize sunmaktadır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 5,
+        poKey: "Oluşturulan rota planları saha personel tarafından mobil uygulama üzerinden görüntülenebilmektedir.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 6,
+        poKey: "Sisteme kaydedilen tüm rotalar seans ve araç bazlı olarak ayrı ayrı sunulmaktadır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 7,
+        poKey: "Oluşturulan güzergâhlara gerektiğinde yeni konumlar eklenerek güncel rota üzerinden planlama yapılabilmektedir.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 8,
+        poKey: "Mobil uygulamadan belirlenen konuma ulaşım bilgisi uygulama üzerinden bildirilmektedir.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    
+    
+];
+export  const benefit = [
+    
+    {
+        id: 0,
+        poKey: "Şirketin araç filosunun/saha personelinin en uygun şekilde yönetilmesini sağlar.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 1,
+        poKey: "Şirket kaynaklarının verimli/etkin kullanılmasını sağlar.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 2,
+        poKey: "Yakıttan tasarruf sağlar.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 3,
+        poKey: "Hizmetlerin belirli bir kalitede ve standartta verilmesini sağlar.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 4,
+        poKey: "Kullanım kolaylığı sayesinde kullanıcı dostudur.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 5,
+        poKey: "Zamandan tasarruf sağlayarak müşterilerinize ve ofis işlerinize odaklanmanızı sağlar.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 6,
+        poKey: "Çevreye zararlı gaz salınımını azaltır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 7,
+        poKey: "Müşteri memnuniyetini arttırır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    {
+        id: 8,
+        poKey: "Süreç yönetimini kolaylaştırır.",
+        icon: <MdOutlineDoubleArrow/>
+    },
+    
+];
