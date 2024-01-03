9539930e403f4e636c2760abc143b6cbdc65b205
Smarteq-Onur <onurkordogan@gmail.com>
2024-01-03 14:35:17 +0300

    kvkk-lang-end

--- components/Kvkk.jsx
+++ components/Kvkk.jsx
@@ -115,25 +115,10 @@ export default function Kvkk() {
           />
           <div className="w-full my-6 px-2 py-6   bg-red-100/10 rounded-lg">
             <h2 className="font-bold text-accent text-center">
-              KİŞİSEL VERİLERİNİZİ TOPLAMAMIZIN YÖNTEMİ VE HUKUKİ SEBEBİ
+            {t(`legal_reason`)}
             </h2>
             <p className="font-semibold">
-              Kişisel verileriniz, Şirketimiz tarafından verilen hizmet, ürün ya
-              da ticari faaliyete bağlı olarak değişkenlik gösterebilmekle; KVK
-              Kanunu m. 4, 5 ve 6’ya uygun bir şekilde, otomatik ya da otomatik
-              olmayan yöntemlerle, ofisler, çağrı merkezi, internet sitesi,
-              sosyal medya mecraları, mobil uygulamalar ve benzeri vasıtalarla
-              sözlü, yazılı ya da elektronik olarak toplanabilecek,
-              güncellenerek işlenebilecektir. Kişisel verileriniz, her türlü
-              sözlü, yazılı ya da elektronik ortamda, yukarıda yer verilen
-              amaçlar doğrultusunda Şirketçe sunduğumuz ürün ve/veya hizmetlerin
-              belirlenen yasal çerçevede sunulabilmesi ve bu kapsamda
-              Şirketimizin sözleşme ve yasadan doğan yükümlülüklerini eksiksiz
-              ve doğru bir şekilde yerine getirebilmesi amacı ile edinilir. Bu
-              hukuki sebeple toplanan kişisel verileriniz KVK Kanunu’nun 5. ve
-              6. maddelerinde belirtilen kişisel veri işleme şartları ve
-              amaçları kapsamında bu Aydınlatma Metninde belirtilen amaçlar
-              dahilinde de işlenebilmekte ve aktarılabilmektedir.
+            {t(`legal_reason_content`)}
             </p>
           </div>
           <div className="w-full flex justify-center relative">
@@ -157,74 +142,26 @@ export default function Kvkk() {
           </div>
           <div className="my-6 px-2 py-6  bg-red-100/10 rounded-lg">
             <h2 className="text-accent font-bold text-center my-2 ">
-              İŞLENEN KİŞİSEL VERİLERİN KİMLERE VE HANGİ AMAÇLA AKTARILABİLECEĞİ
+            {t(`to_whom`)}
             </h2>
             <p className="font-semibold">
-              Şirketimizde KVK Kanunu’na uygun olarak toplanan ve işlenen
-              kişisel verileriniz, sadece işin yapılabilmesi amacıyla ve
-              gizlilik sözleşmeleri eşliğinde; SMARTEQ BİLİŞİM., kişisel
-              verileri çözüm ortaklarına ve sorumlulukların yerine
-              getirilmesinde yardımcı konumundaki şirketlere aktarılabilecektir.
+            {t(`to_whom_content`)}
             </p>
           </div>
           <div className="my-6 px-2 py-6  bg-red-100/10 rounded-lg">
             <h2 className="text-accent font-bold text-center my-2 ">
-              BAŞVURUNUN YAPILMASI
+            {t(`application`)}
             </h2>
             <p className="font-semibold">
-              KVK Kanunu’nun 13. maddesinin 1. fıkrası gereğince, aynı kanunun
-              11. maddesinde belirtilen haklarınızı kullanmak ile ilgili
-              talebinizi kayıtlı elektronik posta (KEP) adresi ya da şirketimize
-              daha önce bildirdiğiniz ve sistemimizde kayıtlı bulunan elektronik
-              posta adresini kullanmak suretiyle, [astorenerji@hs02.kep.tr]
-              adresine ya da yazılı olarak şahsen, noter veya iadeli taahhütlü
-              posta vasıtasıyla iletebilirsiniz. Gerekli açıklamalara ve ilgili
-              forma KVK Başvuru ve Bilgi Talep Formundan erişebilirsiniz.
-              Başvuruda; Adınız, soyadınız ve başvuru yazılı ise imzanız,
-              Türkiye Cumhuriyeti vatandaşı iseniz T.C. kimlik numarası, yabancı
-              iseniz uyruğunuz, pasaport numaranız veya varsa kimlik numaranız,
-              Tebligata esas yerleşim yeri veya iş yeri adresiniz, Varsa
-              bildirime esas elektronik posta adresiniz, telefon ve faks
-              numaranız, Talep konunuzun bulunması gerekmektedir. Konuya ilişkin
-              bilgi ve belgeler başvuruya eklenmelidir.
+            {t(`application_content`)}
             </p>
           </div>
           <div className="my-6 px-2 py-6  bg-red-100/10 rounded-lg">
             <h2 className="text-accent font-bold text-center my-2 ">
-              KİŞİSEL VERİ SAHİBİNİN HAKLARI VE BAŞVURU
+            {t(`data_owner`)} 
             </h2>
             <p className="font-semibold">
-              Kişisel veri sahipleri olarak, KVK Kanunu m. 11’de sayılan
-              haklarınıza ilişkin taleplerinizi, işbu Aydınlatma Metninde
-              aşağıda düzenlenen yöntemlerle Şirketimize iletmeniz durumunda
-              Şirketimiz niteliğine göre, talebi en geç otuz gün içinde ücretsiz
-              olarak sonuçlandıracaktır. Ancak, Kişisel Verileri Koruma
-              Kurulunca bir ücret öngörülmesi halinde, Şirketimiz tarafından
-              belirlenen tarifedeki ücret alınacaktır. Ayrıca yürürlükte olan
-              mevzuat uyarınca kişisel verilere ilişkin başvurular veri
-              sahibinin bizzat kendisi tarafından yapılmak zorunda olduğundan
-              başvurularınızın sadece sizlerle ilgili kısmı cevaplanacak olup,
-              eşiniz, yakınınız ya da arkadaşınız hakkında yapılan bir başvuru
-              kabul edilmeyecektir. Bu kapsamda kişisel veri sahipleri SMARTEQ
-              BİLİŞİM. tarafından internet sayfamızda duyurulan ilgilimize
-              başvurarak kendinizle ilgili olarak; Kişisel veri işlenip
-              işlenmediğini öğrenme, Kişisel verileri işlenmişse buna ilişkin
-              bilgi talep etme, Kişisel verilerin işlenme amacını ve bunların
-              amacına uygun kullanılıp kullanılmadığını öğrenme, Yurt içinde
-              veya yurt dışında kişisel verilerin aktarıldığı üçüncü kişileri
-              bilme, Kişisel verilerin eksik veya yanlış işlenmiş olması hâlinde
-              bunların düzeltilmesini isteme ve bu kapsamda yapılan işlemin
-              kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini
-              isteme, KVK Kanununun ve ilgili diğer kanun hükümlerine uygun
-              olarak işlenmiş olmasına rağmen, işlenmesini gerektiren sebeplerin
-              ortadan kalkması hâlinde kişisel verilerin silinmesini veya yok
-              edilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin
-              aktarıldığı üçüncü kişilere bildirilmesini isteme, İşlenen
-              verilerin münhasıran otomatik sistemler vasıtasıyla analiz
-              edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya
-              çıkmasına itiraz etme, Kişisel verilerin kanuna aykırı olarak
-              işlenmesi sebebiyle zarara uğraması hâlinde zararın giderilmesini
-              talep etme haklarına sahipsiniz.
+            {t(`data_owner_content`)} 
             </p>
           </div>
         </div>
